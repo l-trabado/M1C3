4337f6a4e08a660adabe8d953067d114890a75f5
lt@genII <lautaro.trabado@hotmail.com>
2023-08-27 23:44:31 -0300

    selectorTypeMatcher pasa las pruebas

--- src/selector.js
+++ src/selector.js
@@ -16,6 +16,10 @@ var traverseDomAndCollectElements = function (matchFunc, startEl) {
 
 var selectorTypeMatcher = function (selector) {
   // tu código aquí
+  if (selector.charAt(0) === '#') return "id"
+  if (selector.charAt(0) === '.') return "class"
+  else if (selector.indexOf('.') > 0) return "tag.class"
+  return "tag"
 };
 
 // NOTA SOBRE LA FUNCIÓN MATCH
